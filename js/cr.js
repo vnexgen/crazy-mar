81eec6b4729cea816d5173857b8d9b595d6d2617
laxman <laxman@vnexgen.com>
2017-03-11 10:57:44 +0530

    Design templates are added

---
+++ js/cr.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+    $("#content-slider").lightSlider({
+        item:5,
+        loop:false,
+        keyPress:true,
+        pager:false,
+        slideMove:5,
+        enableDrag:false,
+        slideMargin:20,
+    });
+});
+
+$(document).ready(function() {
+    $("#content-slider1").lightSlider({
+        item:5,
+        loop:false,
+        keyPress:true,
+        pager:false,
+        slideMove:5,
+        enableDrag:false,
+        slideMargin:20,
+    });
+});
+
+$(document).ready(function() {
+    $("#content-slider2").lightSlider({
+        item:5,
+        loop:false,
+        keyPress:true,
+        pager:false,
+        slideMove:5,
+        enableDrag:false,
+        slideMargin:20,
+    });
+});
+
+$(document).ready(function() {
+    $("#content-slider3").lightSlider({
+        item:5,
+        loop:false,
+        keyPress:true,
+        pager:false,
+        slideMove:5,
+        enableDrag:false,
+        slideMargin:20,
+    });
+});
+
+
+
+
+
+
